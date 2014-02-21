a88e98783598170f90d4cecd145992bd7dd75e22
benscott <ben@benscott.co.uk>
2014-02-21 14:15:52 +0000

    Remived log from init

--- ke2sql/__init__.py
+++ ke2sql/__init__.py
@@ -5,45 +5,8 @@ Created by 'bens3' on 2013-06-21.
 Copyright (c) 2013 'bens3'. All rights reserved.
 """
 
-import sys
 import os
-import logging
-from ke2sql.handlers import SQLAlchemyHandler
+from ConfigParser import ConfigParser
 
-__ALL__ = ['log']
-
-def get_logger(name, level=logging.DEBUG):
-    logger = logging.getLogger(name)
-
-    logger.propagate = False
-    formatter = logging.Formatter('%(levelname)s: %(message)s')
-
-    # Output to both log file and stdout
-    file_handler = logging.FileHandler('/var/log/ke2sql.debug.log')
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-        # Output to both log file and stdout
-    file_handler = logging.FileHandler('/var/log/ke2sql.error.log')
-    file_handler.setLevel(logging.ERROR)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-
-    # Add DB log handler
-    db_handler = SQLAlchemyHandler()
-    db_handler.setLevel(logging.CRITICAL)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(db_handler)
-
-    if level:
-        logger.setLevel(level)
-
-    return logger
-
-log = get_logger(__name__)
+config = ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), 'client.cfg'))
