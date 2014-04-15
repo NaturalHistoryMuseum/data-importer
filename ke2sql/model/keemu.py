b0b4d749047afdb949a820313e2cc7f1d9c31de8
benscott <ben@benscott.co.uk>
2014-04-15 10:36:01 +0100

    Fixed list in strings

--- ke2sql/model/keemu.py
+++ ke2sql/model/keemu.py
@@ -119,6 +119,7 @@ class BaseMixin(object):
             column_type = self.get_column_type(name)
 
             # If this is a list for a string field, join together
+            # Only need to do this with Strings - all other field types will raise an exception
             if isinstance(value, list) and isinstance(column_type, String):
                 value = ', '.join(value)
 
