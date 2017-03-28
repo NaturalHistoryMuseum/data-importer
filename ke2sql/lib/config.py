1fb8f7fb5e867d41a6ab9ed4533cd4e273d74f02
benscott <ben@benscott.co.uk>
2017-03-28 14:28:41 +0100

    Python 2 compatible

--- ke2sql/lib/config.py
+++ ke2sql/lib/config.py
@@ -5,7 +5,11 @@ Created by Ben Scott on '20/02/2017'.
 """
 
 import os
-import configparser
+import six
+try:
+    import configparser
+except ImportError:
+    from six.moves import configparser
 
 Config = configparser.ConfigParser()
 Config.read(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config.cfg'))
