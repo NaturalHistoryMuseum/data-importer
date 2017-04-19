a1f1599a3496de719b8472150642181071ee5fab
benscott <ben@benscott.co.uk>
2017-04-19 15:36:24 +0100

    Do not always refresh the view

--- ke2sql/tasks/postgres/view.py
+++ ke2sql/tasks/postgres/view.py
@@ -18,8 +18,7 @@ class MaterialisedViewTask(PostgresQuery):
     """
     Extends Luigi Postgres Query Task for handling materialised views
     """
-
-    rebuild = luigi.BoolParameter(default=False, significant=False)
+    refresh = luigi.BoolParameter(default=True, significant=False)
 
     @abc.abstractproperty
     def views(self):
@@ -41,24 +40,26 @@ class MaterialisedViewTask(PostgresQuery):
         query = []
 
         for view_name, view_sql in self.views:
-            if self.rebuild:
-                db_drop_view(view_name, connection)
-            elif db_view_exists(view_name, connection):
+            # If the view does not exist, create it
+            if not db_view_exists(view_name, connection):
+                logger.info('Creating materialized view %s', view_name)
+                query.append('CREATE MATERIALIZED VIEW "{view_name}" AS ({view_sql})'.format(
+                    view_name=view_name,
+                    view_sql=view_sql
+                ))
+                # Add index on _id column
+                query.append('CREATE UNIQUE INDEX ON "{view_name}"(_id)'.format(
+                    view_name=view_name,
+                ))
+            # Otherwise, refresh the view (unless no refresh is set)
+            elif self.refresh:
                 logger.info('Refreshing materialized view %s', view_name)
                 query.append('REFRESH MATERIALIZED VIEW "{view_name}"'.format(
                     view_name=view_name
                 ))
-                # View exists and is being refreshed, so continue to next view
-                continue
-            logger.info('Creating materialized view %s', view_name)
-            query.append('CREATE MATERIALIZED VIEW "{view_name}" AS ({view_sql})'.format(
-                view_name=view_name,
-                view_sql=view_sql
-            ))
-            # Add index on _id column
-            query.append('CREATE UNIQUE INDEX ON "{view_name}"(_id)'.format(
-                view_name=view_name,
-            ))
-        query = ';'.join(query)
+
+        # If we have queries, join them into one
+        # If no query, use
+        query = ';'.join(query) if query else 'SELECT 1'
         logger.info('Running query: %s', query)
         return query
