a4c358071f81abe015038f5b4349f242bc050d8e
benscott <ben@benscott.co.uk>
2017-05-08 12:22:04 +0100

    Fix unwind to work on live

--- ke2sql/commands/unwind.py
+++ ke2sql/commands/unwind.py
@@ -32,27 +32,64 @@ def unwind():
 
         print('Unwinding %s' % task)
 
-        if task.resource_id != '05ff2255-c38a-40c9-b657-4ccb55ab2feb':
+        # New name of the view
+        view_new_name = '{0}-view'.format(task.resource_id)
+
+        # Check if the renamed view already exists
+        # If it doesn't, we want to rename the resource to use the renamed view
+        if not db_view_exists(view_new_name, connection):
+            # Renamed view does not exist - try and rename it now
+            if not db_view_exists(task.resource_id, connection):
+                raise Exception('Base resource id %s does not exist' % task.resource_id)
+            print('Renaming base view %s' % task.resource_id)
+            cursor.execute('ALTER MATERIALIZED VIEW "{resource_id}" RENAME TO "{view_new_name}"'.format(
+                resource_id=task.resource_id,
+                view_new_name=view_new_name
+            ))
+
+        sql = get_unwind_sql(task, table_name=task.resource_id, view_name=view_new_name)
+
+        # If we have no sql, just continue
+        if not sql:
             continue
 
-        # If the base view doesn't exist, continue to next task
-        if not db_view_exists(task.resource_id, connection):
-            continue
-
-        table_name = "{package_name}".format(
-            package_name=task.package_name,
-        )
-
+        # If the table already exists, drop it
         cursor.execute('DROP TABLE IF EXISTS "{table_name}"'.format(
-            table_name=table_name
+            table_name=task.resource_id
         ))
+        # Execute the SQL to create another table
+        # print(sql)
+        cursor.execute(sql)
 
-        # Get the properties to insert - excluding emultimedia as these won't be added
-        # to the properties - there are in their own jsonb collection
-        properties = [(f.module_name, f.field_alias) for f in task.fields if f.module_name != 'emultimedia']
-        # Dedupe properties
-        properties = list(set(properties))
+        # Add some indexes
+        indexes = [
+            'ALTER TABLE "{table_name}" ADD PRIMARY KEY (_id)'.format(
+                table_name=task.resource_id
+            )
+        ]
+        for indexed_field in get_indexed_fields(task):
+            indexes.append('CREATE INDEX ON "{table_name}"("{field_name}")'.format(
+                table_name=task.resource_id,
+                field_name=indexed_field
+            ))
 
+        # Execute indexes SQL
+        cursor.execute(';'.join(indexes))
+
+    connection.commit()
+
+
+def get_unwind_sql(task, table_name, view_name):
+    """
+    Get unwinding SQL for a particular task
+    """
+    # Get the properties to insert - excluding emultimedia as these won't be added
+    # to the properties - there are in their own jsonb collection
+    properties = [(f.module_name, f.field_alias) for f in task.fields if f.module_name != 'emultimedia']
+    # Dedupe properties
+    properties = list(set(properties))
+
+    if task.resource_id == '05ff2255-c38a-40c9-b657-4ccb55ab2feb':
         # Collection code is truncated
         properties.remove(('ecatalogue', 'collectionCode'))
         properties.remove(('ecatalogue', 'dateModified'))
@@ -68,21 +105,19 @@ def unwind():
             ('ecatalogue', 'collectionCode')
         ])
 
-        indexed_fields = ['collectionCode', 'catalogNumber', 'created', 'project']
-
-        properties_select = list(map(lambda p: 'CAST("{0}".properties->>\'{1}\' AS CITEXT) as "{1}"'.format(task.resource_id, p[1]), properties))
+        properties_select = list(map(lambda p: 'CAST("{0}".properties->>\'{1}\' AS CITEXT) as "{1}"'.format(view_name, p[1]), properties))
 
         sql = """CREATE TABLE "{table_name}" AS (
             SELECT
-                "{resource_id}"._id,
-                cast("{resource_id}".properties->>'occurrenceID' AS UUID) as "occurrenceID",
-                "{resource_id}"."associatedMedia"::text,
-                "{resource_id}".properties->>'dateCreated' as created,
-                "{resource_id}".properties->>'dateModified' as modified,
-                cast("{resource_id}".properties->>'decimalLongitude' AS FLOAT8) as "decimalLongitude",
-                cast("{resource_id}".properties->>'decimalLatitude' AS FLOAT8) as "decimalLatitude",
-                "{resource_id}"._geom,
-                "{resource_id}"._the_geom_webmercator,
+                "{view_name}"._id,
+                cast("{view_name}".properties->>'occurrenceID' AS UUID) as "occurrenceID",
+                "{view_name}"."associatedMedia"::text,
+                "{view_name}".properties->>'dateCreated' as created,
+                "{view_name}".properties->>'dateModified' as modified,
+                cast("{view_name}".properties->>'decimalLongitude' AS FLOAT8) as "decimalLongitude",
+                cast("{view_name}".properties->>'decimalLatitude' AS FLOAT8) as "decimalLatitude",
+                "{view_name}"._geom,
+                "{view_name}"._the_geom_webmercator,
                 'Specimen' as "recordType",
                 {properties_select},
                 NULL::TEXT as "maxError",
@@ -91,29 +126,21 @@ def unwind():
                 NULL::TEXT as "relatedResourceID",
                 FALSE as centroid,
                   ''::tsvector as _full_text
-                FROM "{resource_id}"
-                WHERE "{resource_id}".properties->>'occurrenceID' != ')')
+                FROM "{view_name}"
+                WHERE "{view_name}".properties->>'occurrenceID' != ')')
         """.format(
             table_name=table_name,
-            resource_id=task.resource_id,
+            view_name=view_name,
             properties_select=','.join(properties_select)
         )
-        cursor.execute(sql)
-        indexes = [
-            'ALTER TABLE "{table_name}" ADD PRIMARY KEY (_id)'.format(
-                table_name=table_name
-            )
-        ]
-        for indexed_field in indexed_fields:
-            indexes.append('CREATE INDEX ON "{table_name}"("{field_name}")'.format(
-                table_name=table_name,
-                field_name=indexed_field
-            ))
 
-        # Execute indexes SQL
-        cursor.execute(';'.join(indexes))
+        return sql
 
-    connection.commit()
+
+def get_indexed_fields(task):
+    if task.resource_id == '05ff2255-c38a-40c9-b657-4ccb55ab2feb':
+        return ['collectionCode', 'catalogNumber', 'created', 'project']
+    return []
 
 if __name__ == "__main__":
     unwind()
