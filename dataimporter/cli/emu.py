46172cbf171e77f121fa5a1763b267d2ce9fcf28
Josh Humphries <j.humphries@nhm.ac.uk>
2024-05-15 17:43:08 +0100

    chore: do some minor fiddling with emu cli

--- dataimporter/cli/emu.py
+++ dataimporter/cli/emu.py
@@ -1,3 +1,5 @@
+import math
+
 import click
 
 from dataimporter.cli.utils import with_config, console, VIEW_NAMES
@@ -53,7 +55,7 @@ def auto(config: Config, one: bool = False):
 
             for name in VIEW_NAMES:
                 console.log(f"Syncing changes from {name} view to elasticsearch")
-                importer.sync_to_elasticsearch(name, parallel=True)
+                importer.sync_to_elasticsearch(name)
                 console.log(f"Finished with {name}")
 
             count += 1
@@ -65,46 +67,83 @@ def auto(config: Config, one: bool = False):
                     console.log(f"Forced merge on {name}")
 
 
-@emu_group.command("queue-next")
+@emu_group.command()
+@click.argument("amount", type=str)
 @with_config()
-def queue_next(config: Config):
+def queue(amount: str, config: Config):
     """
-    Queues the next day's data and does nothing else.
+    Queues the specified amount of EMu exports.
+
+    This can be "next", "all", or a positive number.
     """
+    if amount == "next":
+        amount = 1
+        console.log(f"Queuing next dumpset only")
+    elif amount == "all":
+        amount = math.inf
+        console.log(f"Queuing all available dumpsets")
+    else:
+        amount = int(amount)
+        assert amount > 0, "Amount must be greater than 0"
+        console.log(f"Queuing next {amount} dumpsets")
+
     with DataImporter(config) as importer:
-        dates_queued = importer.queue_emu_changes(only_one=True)
-        if not dates_queued:
-            console.log("No data to queue")
-        else:
-            date_queued = dates_queued[0].isoformat()
-            console.log(f"Date queued: {date_queued}")
+        console.log(f"Current latest queued dumpset date: {importer.emu_status.get()}")
+
+        while amount > 0:
+            console.log("Queuing next dump set")
+            dates_queued = importer.queue_emu_changes(only_one=True)
+            if not dates_queued:
+                console.log("No data to queue")
+                break
+            else:
+                date_queued = dates_queued[0].isoformat()
+                console.log(f"Date queued: {date_queued}")
+            amount -= 1
 
 
 @emu_group.command("update-mongo")
+@click.argument("view", type=click.Choice(VIEW_NAMES))
+@click.option(
+    "--everything",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Add all records to MongoDB regardless of whether they have changed. This is "
+    "primarily useful when a change has been made to a view's filters or "
+    "representation",
+)
 @with_config()
-def update_mongo(config: Config):
+def update_mongo(view: str, config: Config, everything: bool = False):
     """
-    Updates MongoDB with any queued EMu changes and flushes the queues.
+    On the given view, updates MongoDB with any queued EMu changes and flushes the
+    queues.
     """
     with DataImporter(config) as importer:
-        for name in VIEW_NAMES:
-            console.log(f"Adding changes from {name} view to mongo")
-            importer.add_to_mongo(name)
-            console.log(f"Added changes from {name} view to mongo")
-
+        console.log(f"Adding changes from {view} view to mongo")
+        importer.add_to_mongo(view, everything=everything)
+        console.log(f"Added changes from {view} view to mongo")
         console.log(f"Flushing queues")
         importer.flush_queues()
         console.log(f"Flushed queues")
 
 
 @emu_group.command("update-es")
+@click.argument("view", type=click.Choice(VIEW_NAMES))
+@click.option(
+    "--resync",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Resynchronise all data in Elasticsearch with MongoDB for this view, not just "
+    "the records that have changed.",
+)
 @with_config()
-def update_es(config: Config):
+def update_es(view: str, config: Config, resync: bool = False):
     """
-    Updates Elasticsearch with the changes in MongoDB.
+    Updates Elasticsearch with the changes in MongoDB for the given view.
     """
     with DataImporter(config) as importer:
-        for name in VIEW_NAMES:
-            console.log(f"Syncing changes from {name} view to elasticsearch")
-            importer.sync_to_elasticsearch(name, parallel=True)
-            console.log(f"Finished with {name}")
+        console.log(f"Syncing changes from {view} view to elasticsearch")
+        importer.sync_to_elasticsearch(view, resync=resync)
+        console.log(f"Finished with {view}")
