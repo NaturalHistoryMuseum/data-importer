25520028438a9c710c31aea03e5a0f5bbf1b778b
Josh Humphries <j.humphries@nhm.ac.uk>
2025-04-17 16:00:34 +0100

    fix: remove new mammal parts field from specimen
    We haven't been through this fully yet with the curators so better to release this later.

--- dataimporter/emu/views/specimen.py
+++ dataimporter/emu/views/specimen.py
@@ -396,11 +396,4 @@ class SpecimenView(View):
         # add GBIF
         merge(record, data, self.gbif_link)
 
-        # add mammal part summary
-        mammal_parts = self.mammal_part_link.lookup_and_transform(record)
-        if mammal_parts:
-            parts = [part["part"] for part in mammal_parts]
-            # todo: field name ok?
-            data["preparations"] = parts[0] if len(parts) == 1 else parts
-
         return data
